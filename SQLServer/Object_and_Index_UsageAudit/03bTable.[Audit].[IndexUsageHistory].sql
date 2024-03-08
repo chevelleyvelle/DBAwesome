9c1aa38cbb81ac130070e5d635d06c6ac032ba86
Chevelle <55494532+chevelleyvelle@users.noreply.github.com>
2024-03-08 16:29:58 -0500

    Update 03bTable.[Audit].[IndexUsageHistory].sql

--- SQLServer/Object_and_Index_UsageAudit/03bTable.[Audit].[IndexUsageHistory].sql
+++ SQLServer/Object_and_Index_UsageAudit/03bTable.[Audit].[IndexUsageHistory].sql
@@ -1,7 +1,7 @@
 USE [DbMaintenance]
 GO
 
-/****** Object:  Table [Audit].[IndexUsage]    Script Date: 6/22/2021 6:23:04 PM ******/
+/****** Object:  Table [Audit].[IndexUsageHistory]    Script Date: 3/8/2024 2:35:47 PM ******/
 SET ANSI_NULLS ON
 GO
 
@@ -9,47 +9,47 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 CREATE TABLE [Audit].[IndexUsageHistory](
-	[ID] [int] IDENTITY(1,1) NOT NULL,
-	[IndexUsageID] [int] NOT NULL,
-	[DBName] [nvarchar](128) NOT NULL,
-	[ObjectID] [int] NOT NULL,
-	[SchemaName] [nvarchar](128) NULL,
-	[TableName] [nvarchar](128) NULL,
-	[IndexID] [int] NOT NULL,
-	[IndexName] [nvarchar](128) NOT NULL,
-	[IndexColumns] [nvarchar](max) NULL,
-	[IncludeColumns] [nvarchar](max) NULL,
-	[IndexFilter] [nvarchar](max) NULL,
-	[IndexType] [nvarchar](60) NULL,
-	[IsPrimaryKey] [bit] NULL,
-	[IsClustered] [bit] NULL,
-	[IsUnique] [bit] NULL,
-	[IsUniqueConstraint] [bit] NULL,
-	[HasFilter] [bit] NULL,
-	[IsDisabled] [bit] NULL,
-	[IndexSizeKB] [bigint] NULL,
-	[TotalSeeks] [bigint] NULL,
-	[TotalScans] [bigint] NULL,
-	[TotalLookups] [bigint] NULL,
-	[TotalUpdates] [bigint] NULL,
-	[LastUserSeek] [datetime] NULL,
-	[LastUserScan] [datetime] NULL,
-	[LastUserLookup] [datetime] NULL,
-	[LastUserUpdate] [datetime] NULL,
-	[ExcludeFromCleanup] [bit] NOT NULL,
-	[IsDeleted] [bit] NOT NULL,
-	[EmailSendDate] [datetime] NULL,
-	[DisableIndexDate] [datetime] NULL,
-	[DisableIndexCommand] [nvarchar](4000) NULL,
-	[RebuildIndexCommand] [nvarchar](4000) NULL,
-	[CleanupDate] [datetime] NULL,
-	[CleanupCommand] [nvarchar](4000) NULL,
-	[RollbackDate] [datetime] NULL,
-	[RollbackCommand] [nvarchar](max) NULL,
-	[InsertDate] [datetime] NOT NULL,
-	[ModifyDate] [datetime] NOT NULL,
-	[SQLRestartDate][datetime] NULL,
-	[HistoryInsertDate] [datetime] NOT NULL
+	[ID] [INT] IDENTITY(1,1) NOT NULL,
+	[IndexUsageID] [INT] NOT NULL,
+	[DBName] [NVARCHAR](128) NOT NULL,
+	[ObjectID] [INT] NOT NULL,
+	[SchemaName] [NVARCHAR](128) NULL,
+	[TableName] [NVARCHAR](128) NULL,
+	[IndexID] [INT] NOT NULL,
+	[IndexName] [NVARCHAR](128) NOT NULL,
+	[IndexColumns] [NVARCHAR](MAX) NULL,
+	[IncludeColumns] [NVARCHAR](MAX) NULL,
+	[IndexFilter] [NVARCHAR](MAX) NULL,
+	[IndexType] [NVARCHAR](60) NULL,
+	[IsPrimaryKey] [BIT] NULL,
+	[IsClustered] [BIT] NULL,
+	[IsUnique] [BIT] NULL,
+	[IsUniqueConstraint] [BIT] NULL,
+	[HasFilter] [BIT] NULL,
+	[IsDisabled] [BIT] NULL,
+	[IndexSizeKB] [BIGINT] NULL,
+	[TotalSeeks] [BIGINT] NULL,
+	[TotalScans] [BIGINT] NULL,
+	[TotalLookups] [BIGINT] NULL,
+	[TotalUpdates] [BIGINT] NULL,
+	[LastUserSeek] [DATETIME] NULL,
+	[LastUserScan] [DATETIME] NULL,
+	[LastUserLookup] [DATETIME] NULL,
+	[LastUserUpdate] [DATETIME] NULL,
+	[ExcludeFromCleanup] [BIT] NOT NULL,
+	[IsDeleted] [BIT] NOT NULL,
+	[EmailSendDate] [DATETIME] NULL,
+	[DisableIndexDate] [DATETIME] NULL,
+	[DisableIndexCommand] [NVARCHAR](4000) NULL,
+	[RebuildIndexCommand] [NVARCHAR](4000) NULL,
+	[CleanupDate] [DATETIME] NULL,
+	[CleanupCommand] [NVARCHAR](4000) NULL,
+	[RollbackDate] [DATETIME] NULL,
+	[RollbackCommand] [NVARCHAR](MAX) NULL,
+	[InsertDate] [DATETIME] NOT NULL,
+	[ModifyDate] [DATETIME] NOT NULL,
+	[SQLRestartDate] [DATETIME] NULL,
+	[HistoryInsertDate] [DATETIME] NOT NULL,
  CONSTRAINT [PK_IndexUsageHistory_ID] PRIMARY KEY CLUSTERED 
 (
 	[ID] ASC
@@ -57,7 +57,11 @@ CREATE TABLE [Audit].[IndexUsageHistory](
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
 
-ALTER TABLE [Audit].[IndexUsageHistory] ADD  CONSTRAINT [DF_IndexUsageHistory_HistoryInsertDate]  DEFAULT (getdate()) FOR [HistoryInsertDate]
+ALTER TABLE [Audit].[IndexUsageHistory] ADD  CONSTRAINT [DF_IndexUsageHistory_HistoryInsertDate]  DEFAULT (GETDATE()) FOR [HistoryInsertDate]
 GO
 
-CREATE NONCLUSTERED INDEX IX_IndexUsageHistory_HistoryInsertDate ON [Audit].IndexUsageHistory(HistoryInsertDate) WITH (ONLINE = ON);
+CREATE NONCLUSTERED INDEX [IX_IndexUsageHistory_HistoryInsertDate] ON [Audit].[IndexUsageHistory]
+(
+	[HistoryInsertDate] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
