29b893d7804903a424759207c4315febf44365f7
Chevelle <55494532+chevelleyvelle@users.noreply.github.com>
2024-03-08 16:29:22 -0500

    Update 03.Table.[Audit].[IndexUsage].sql

--- SQLServer/Object_and_Index_UsageAudit/03.Table.[Audit].[IndexUsage].sql
+++ SQLServer/Object_and_Index_UsageAudit/03.Table.[Audit].[IndexUsage].sql
@@ -1,7 +1,7 @@
 USE [DbMaintenance]
 GO
 
-/****** Object:  Table [Audit].[IndexUsage]    Script Date: 6/16/2021 4:14:29 PM ******/
+/****** Object:  Table [Audit].[IndexUsage]    Script Date: 3/7/2024 10:03:35 AM ******/
 SET ANSI_NULLS ON
 GO
 
@@ -9,45 +9,49 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 CREATE TABLE [Audit].[IndexUsage](
-	[ID] [int] IDENTITY(1,1) NOT NULL,
-	[DBName] [nvarchar](128) NOT NULL,
-	[ObjectID] [int] NOT NULL,
-	[SchemaName] [nvarchar](128) NULL,
-	[TableName] [nvarchar](128) NULL,
-	[IndexID] [int] NOT NULL,
-	[IndexName] [nvarchar](128) NOT NULL,
-	[IndexColumns] [nvarchar](max) NULL,
-	[IncludeColumns] [nvarchar](max) NULL,
-	[IndexFilter] [nvarchar](max) NULL,
-	[IndexType] [nvarchar](60) NULL,
-	[IsPrimaryKey] [bit] NULL,
-	[IsClustered] [bit] NULL,
-	[IsUnique] [bit] NULL,
-	[IsUniqueConstraint] [bit] NULL,
-	[HasFilter] [bit] NULL,
-	[IsDisabled] [bit] NULL,
-	[IndexSizeKB] [bigint] NULL,
-	[TotalSeeks] [bigint] NULL,
-	[TotalScans] [bigint] NULL,
-	[TotalLookups] [bigint] NULL,
-	[TotalUpdates] [bigint] NULL,
-	[LastUserSeek] [datetime] NULL,
-	[LastUserScan] [datetime] NULL,
-	[LastUserLookup] [datetime] NULL,
-	[LastUserUpdate] [datetime] NULL,
-	[ExcludeFromCleanup] [bit] NOT NULL,
-	[IsDeleted] [bit] NOT NULL,
-	[EmailSendDate] [datetime] NULL,
-	[DisableIndexDate] [datetime] NULL,
-	[DisableIndexCommand] [nvarchar](4000) NULL,
-	[RebuildIndexCommand] [nvarchar](4000) NULL,
-	[CleanupDate] [datetime] NULL,
-	[CleanupCommand] [nvarchar](4000) NULL,
-	[RollbackDate] [datetime] NULL,
-	[RollbackCommand] [nvarchar](max) NULL,
-	[InsertDate] [datetime] NOT NULL,
-	[ModifyDate] [datetime] NOT NULL,
-	[SQLRestartDate] [datetime] NULL,
+	[ID] [INT] IDENTITY(1,1) NOT NULL,
+	[DBName] [NVARCHAR](128) NOT NULL,
+	[ObjectID] [INT] NOT NULL,
+	[SchemaName] [NVARCHAR](128) NULL,
+	[TableName] [NVARCHAR](128) NULL,
+	[IndexID] [INT] NOT NULL,
+	[IndexName] [NVARCHAR](128) NOT NULL,
+	[IndexColumns] [NVARCHAR](MAX) NULL,
+	[IncludeColumns] [NVARCHAR](MAX) NULL,
+	[IndexFilter] [NVARCHAR](MAX) NULL,
+	[IndexType] [NVARCHAR](60) NULL,
+	[IsPrimaryKey] [BIT] NULL,
+	[IsClustered] [BIT] NULL,
+	[IsUnique] [BIT] NULL,
+	[IsUniqueConstraint] [BIT] NULL,
+	[HasFilter] [BIT] NULL,
+	[IsDisabled] [BIT] NULL,
+	[IsCompressed] [BIT] NULL,
+	[CompressionDescription] [NVARCHAR](60) NULL,
+	[IndexSizeKB] [BIGINT] NULL,
+	[LobDataSizeKB] [BIGINT] NULL,
+	[RowCount] [BIGINT] NULL,
+	[TotalSeeks] [BIGINT] NULL,
+	[TotalScans] [BIGINT] NULL,
+	[TotalLookups] [BIGINT] NULL,
+	[TotalUpdates] [BIGINT] NULL,
+	[LastUserSeek] [DATETIME] NULL,
+	[LastUserScan] [DATETIME] NULL,
+	[LastUserLookup] [DATETIME] NULL,
+	[LastUserUpdate] [DATETIME] NULL,
+	[ExcludeFromCleanup] [BIT] NOT NULL,
+	[IsDeleted] [BIT] NOT NULL,
+	[EmailSendDate] [DATETIME] NULL,
+	[DisableIndexDate] [DATETIME] NULL,
+	[DisableIndexCommand] [NVARCHAR](4000) NULL,
+	[RebuildIndexCommand] [NVARCHAR](4000) NULL,
+	[CleanupDate] [DATETIME] NULL,
+	[CleanupCommand] [NVARCHAR](4000) NULL,
+	[RollbackDate] [DATETIME] NULL,
+	[RollbackCommand] [NVARCHAR](MAX) NULL,
+	[InsertDate] [DATETIME] NOT NULL,
+	[ModifyDate] [DATETIME] NOT NULL,
+	[SQLRestartDate] [DATETIME] NULL,
  CONSTRAINT [PK_IndexUsage_ID] PRIMARY KEY CLUSTERED 
 (
 	[ID] ASC
@@ -61,10 +65,8 @@ GO
 ALTER TABLE [Audit].[IndexUsage] ADD  CONSTRAINT [DF_IndexUsage_IsDeleted]  DEFAULT ((0)) FOR [IsDeleted]
 GO
 
-ALTER TABLE [Audit].[IndexUsage] ADD  CONSTRAINT [DF_IndexUsage_InsertDate]  DEFAULT (getdate()) FOR [InsertDate]
+ALTER TABLE [Audit].[IndexUsage] ADD  CONSTRAINT [DF_IndexUsage_InsertDate]  DEFAULT (GETDATE()) FOR [InsertDate]
 GO
 
-ALTER TABLE [Audit].[IndexUsage] ADD  CONSTRAINT [DF_IndexUsage_ModifyDate]  DEFAULT (getdate()) FOR [ModifyDate]
+ALTER TABLE [Audit].[IndexUsage] ADD  CONSTRAINT [DF_IndexUsage_ModifyDate]  DEFAULT (GETDATE()) FOR [ModifyDate]
 GO
-
-
